81e567203eb013f3794ca876f4e49aeea95653ad
Pierre-Alexis Blond <34747791+PABlond@users.noreply.github.com>
2022-07-31 23:55:18 +0200

    add second route

--- server/routes/exercices.js
+++ server/routes/exercices.js
@@ -5,11 +5,12 @@ import { getPairsController } from "./exercices.controller";
 const router = express.Router();
 
 router.get("/1", async (req, res) => {
-  
   const { USDC, USDT } = addresses.tokens;
   return res.json(await getPairsController(USDT, USDC));
 });
 
+router.get("/2", async (req, res) => {
+  return res.json("Depuis la paire USDT/USDC sur trader joe, appeler la fonction getReserves et afficher le résultat ici");
+});
+
 export default router;
-
-
